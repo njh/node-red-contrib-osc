a6299a5a24230e4895cf956806fdeaf6eaa9735b
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2016-05-08 22:27:04 -0400

    Small fix

--- osc.js
+++ osc.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 Nicholas Humfrey
+ * Copyright 2016 Nicholas Humfrey, Nathanaël Lécaudé
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
